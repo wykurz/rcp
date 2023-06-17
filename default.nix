bc8fbe84349a28bd4601e384fc1c8469f792f8d5
Mateusz Wykurz <wykurz@gmail.com>
2023-06-17 14:05:26 -0600

    Initial commit

---
+++ default.nix
@@ -0,0 +1,28 @@
+let
+  rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  myrust = nixpkgs.rust-bin.stable."1.70.0".default.override {
+    extensions = [ "rust-analysis" "rust-src" ];
+  };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "rust-shell";
+    buildInputs = [
+      rust-analyzer
+      myrust
+      binutils
+      cargo-audit
+      cargo-bloat
+      cargo-deny
+      cargo-edit
+      cargo-expand
+      cargo-flamegraph
+      cargo-generate
+      cargo-outdated
+      cargo-udeps
+      gdb
+      llvmPackages.bintools
+    ];
+    RUST_SRC_PATH = "${myrust}/lib/rustlib/src/rust/src";
+  }
