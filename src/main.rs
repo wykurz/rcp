290304416ff52588217c38cbb2b099e66f701889
Mateusz Wykurz <wykurz@gmail.com>
2023-06-17 14:32:41 -0600

    Setup rust binary and project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+use anyhow::Result;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rcp")]
+struct Args {
+    /// Output file
+    #[structopt(short, long, parse(from_os_str))]
+    src: std::path::PathBuf,
+
+    /// Output file
+    #[structopt(short, long, parse(from_os_str))]
+    dst: std::path::PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+    let args = Args::from_args();
+    info!("Copy: {:?} -> {:?}", args.src, args.dst);
+    Ok(())
 }
