c42c6fdf57908fa193b74bdc9fec018cd0f8c1e3
Mateusz Wykurz <wykurz@gmail.com>
2023-08-31 15:48:58 -0600

    Update default rust version to 1.72

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  myrust = nixpkgs.rust-bin.stable."1.71.0".default.override {
+  myrust = nixpkgs.rust-bin.stable."1.72.0".default.override {
     extensions = [ "rust-analysis" "rust-src" ];
   };
 in
