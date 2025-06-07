4c52d802bf0ea7a4a446c22096dcdcf08589de46
Mateusz Wykurz <wykurz@gmail.com>
2025-06-07 15:03:06 -0600

    Minor cleanups

--- rcpd/src/source.rs
+++ rcpd/src/source.rs
@@ -1,5 +1,3 @@
-use anyhow::Context;
-use rand::{distributions::Alphanumeric, Rng};
 use tracing::{instrument, Level};
 
 // FIXME
@@ -35,29 +33,19 @@ pub async fn run_source(
     master_connection: &quinn::Connection,
     max_concurrent_streams: u32,
 ) -> anyhow::Result<String> {
-    let server_config = remote::configure_server(max_concurrent_streams)?;
-    let addr = "0.0.0.0:0".parse::<std::net::SocketAddr>().unwrap();
-    let endpoint =
-        quinn::Endpoint::server(server_config, addr).context("Failed to create QUIC endpoint")?;
-    let bound_addr = endpoint
-        .local_addr()
-        .context("Failed to get local address")?;
-    tracing::event!(Level::INFO, "QUIC server listening on {}", bound_addr);
-    let server_name: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(20)
-        .map(char::from)
-        .collect();
-    let local_ip = remote::get_local_ip().context("Failed to get local IP address")?;
+    let server_endpoint = remote::get_server(max_concurrent_streams)?;
+    let server_addr = remote::get_endpoint_addr(&server_endpoint)?;
+    tracing::event!(Level::INFO, "QUIC server listening on {}", server_addr);
+    let server_name = remote::get_random_server_name();
     let master_hello = remote::protocol::SourceMasterHello {
-        source_addr: std::net::SocketAddr::new(local_ip, bound_addr.port()),
+        source_addr: server_addr,
         server_name: server_name.clone(),
     };
     tracing::event!(Level::INFO, "Sending master hello: {:?}", master_hello);
     let master_hello = bincode::serialize(&master_hello)?;
     master_connection.send_datagram(bytes::Bytes::from(master_hello))?;
     // start accepting connections from destination
-    if let Some(conn) = endpoint.accept().await {
+    if let Some(conn) = server_endpoint.accept().await {
         tracing::event!(Level::INFO, "New QUIC connection incoming");
         handle_quic_connection(conn).await?;
     }
