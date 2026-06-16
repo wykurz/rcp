cad8f01a9911b25fc410641b9e65bfd71858ed25
Mateusz Wykurz <wykurz@gmail.com>
2026-06-16 09:02:00 -0600

    Bump version to 0.36.0

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
         # Package builder for RCP tools with custom binary names
         mkRcpPackage = { packageName, binaryName, description }: pkgs.rustPlatform.buildRustPackage {
           pname = binaryName;
-          version = "0.35.0";
+          version = "0.36.0";
           src = ./.;
 
           cargoLock = {
@@ -117,7 +117,7 @@
           # All tools in one package
           rcp-all = pkgs.rustPlatform.buildRustPackage {
             pname = "rcp-all";
-            version = "0.35.0";
+            version = "0.36.0";
             src = ./.;
 
             cargoLock = {
